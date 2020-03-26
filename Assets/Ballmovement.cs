f88446ac4f77b59f6d02e1d90a5660cf510b67a5
Cut0000 <cuthusnul12@gmail.com>
2020-03-26 23:01:47 +0700

    revisi ball movement

---
+++ Assets/Ballmovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballmovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public int speed=20;
+    public int speed = 30;
+
+    public Rigidbody2D sesuatu;
+
+    void Start()
+    {
+        //Debug.Log("Hello Word");
+        //GetComponent<Rigidbody2D>().velocity = new Vector2 (1,1) * speed;
+        sesuatu.velocity = new Vector2(-1,-1) * speed;
+
+    }
+
+    // Update is called once per frame
+    //void FixedUpdate()
+    void Update()
+    {
+
+        //Update is called once per frame void Fixed update
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+      if(other.collider.name == "Wallkanan" || other.collider.name == "WallKiri"){
+          GetComponent<Transform>().position = new Vector2 (0,0);
+        
+        }
+    }
+}
